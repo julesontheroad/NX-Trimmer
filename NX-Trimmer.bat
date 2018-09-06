51be7bd4a9c36c33ab8404d81b23f23c532dd370
julesontheroad <42461174+julesontheroad@users.noreply.github.com>
2018-09-06 17:17:20 +0200

    Add files via upload

--- NX-Trimmer.bat
+++ NX-Trimmer.bat
@@ -1,7 +1,7 @@
 @echo off
 color 03
 
-::NX-Trimmer v0.3.1 by julesontheroad::
+::NX-Trimmer v0.4 by julesontheroad::
 ::A batch file made to automate system updates trimming, game updates trimming and padding reduction via hacbuild
 ::NX-Trimmer serves as a workflow helper for hacbuild, hactool and nspbuild
 ::Just drag and drop. Drag a xci file into NX-Trimmer .bat and it'll take care of everything
@@ -29,27 +29,51 @@ set /a preservemanual=0
 set /a delete_brack_tags=1
 set /a delete_pa_tags=0
 
-
+CD /d "%~dp0"
+if not exist "ztools" goto iztools
 ::check for keys.txt
-if not exist "%~dp0\ztools\keys.txt" echo Falta archivo "KEYS.txt" o "keys.txt!"
+if not exist "ztools\keys.txt" echo Missing file "KEYS.txt" or "keys.txt!"
 echo.
-if not exist "%~dp0\ztools\keys.txt" pause
-if not exist "%~dp0\ztools\keys.txt" exit
+if not exist "ztools\keys.txt" pause
+if not exist "ztools\keys.txt" exit
+goto startpr
+
+:iztools
+CD /d ".."
+if not exist "ztools\keys.txt" echo Missing file "KEYS.txt" or "keys.txt!"
+echo.
+if not exist "ztools\keys.txt" pause
+if not exist "ztools\keys.txt" exit
+goto startpr
+
+:startpr
 
 ::Set working folder and file
 set file=%~n1
 FOR %%i IN ("%file%") DO (
 set filename=%%~ni
 )
-CD /d "%~dp0"
+
 if exist "xciDecrypted" RD /s /q "xciDecrypted"
 cls
 
 if "%~x1"==".xci" ( goto xci )
-if "%~x1"==".*" ( goto salida )
+if "%~x1"==".nsp" ( goto end )
+goto end 
+
+ECHO ------------------------------------------
+ECHO ====== NX-TRIMMER BY JULESONTHEROAD ====== 
+ECHO ------------------------------------------
+ECHO "            VERSION 0.4                 "
+ECHO more in: https://github.com/julesontheroad/
+echo.
+ECHO ............
+ECHO INSTRUCTIONS
+ECHO ............
+echo.
 echo This program works dragging an xci file over it's bat file
 echo.
-echo It also works with NX-Trimmer_v0.3.1.bat "filename.xci" over the command line
+echo It also works with NX-Trimmer.bat "filename.xci" over the command line
 echo.
 echo It's function is to automate update partition removal and padding reduction via hacbuild
 echo.
@@ -58,28 +82,23 @@ echo.
 pause
 exit
 
-:salida
-echo This program only accepts xci files.
-echo.
-pause
-exit
-
 :xci
 @echo off
 setlocal enabledelayedexpansion
-if exist "xciDecrypted" RD /s /q "xciDecrypted"
+
+if exist "xciDecrypted" RD /s /q "xciDecrypted" >NUL 2>&1
 if not exist game_info MD game_info
 if not exist output_nxt MD output_nxt
 MD xciDecrypted
 
-if exist "game_info\!filename!.ini" del "game_info\!filename!.ini"
-cls
-"%~dp0\ztools\hacbuild.exe" read xci "%~f1"
-cls
+if exist "game_info\!filename!.ini" del "game_info\!filename!.ini" >NUL 2>&1
 
-move  "%~dp1\*.ini"  "%~dp0\xciDecrypted\" 
+"ztools\hacbuild.exe" read xci "%~f1" >NUL 2>&1
+
+
+move  "%~dp1\*.ini"  "xciDecrypted\" 
 echo f | xcopy /f /y "xciDecrypted\*.ini" "game_info\"
-RENAME xciDecrypted\*.ini ""game_info.ini"
+rename xciDecrypted\*.ini ""game_info.ini"
 
 :: If activated remove identifiers [] or () in filename.
 if exist xciDecrypted\*.txt del xciDecrypted\*.txt
@@ -103,15 +122,27 @@ if exist xciDecrypted\fname.txt del xciDecrypted\fname.txt
 
 ::I also wanted to remove_(
 set filename=%filename:_= %
-if exist "%~dp0\output_nxt\!filename!" RD /s /q "%~dp0\output_nxt\!filename!"
 cls
+
+ECHO ------------------------------------------
+ECHO ====== NX-TRIMMER BY JULESONTHEROAD ====== 
+ECHO ------------------------------------------
+ECHO "            VERSION 0.4                 "
+ECHO more in: https://github.com/julesontheroad/
+PING -n 2 127.0.0.1 >NUL 2>&1
 echo STARTING PROCESS... 
+echo PROCESSING  !filename!
 echo Please wait till window closes
 echo Depending on file size it can take a little
+PING -n 2 127.0.0.1 >NUL 2>&1
 
+if exist "output_nxt\!filename!" RD /s /q "output_nxt\!filename!" >NUL 2>&1
 ::Get partitions table from hactool and format it to get the secure partitions nca in correct order
-"%~dp0\ztools\hactool.exe" -k "%~dp0\ztools\keys.txt" -t xci -i  "%~1" >xciDecrypted\partitions.txt 
-cls
+echo -----------------------------------------------------
+echo Getting partition information with hactool by SciresM
+echo -----------------------------------------------------
+"ztools\hactool.exe" -k "ztools\keys.txt" -t xci -i  "%~1" >xciDecrypted\partitions.txt 
+echo DONE
 
 for /f "tokens=2* delims= " %%a in (xciDecrypted\partitions.txt) do (
 echo %%a>>xciDecrypted\helper1.txt
@@ -123,17 +154,17 @@ echo %%a>>xciDecrypted\helper2.txt
 )
 FINDSTR "secure:/" xciDecrypted\helper2.txt >>xciDecrypted\s_order.txt
 
-for /f "tokens=4* delims= " %%a in (xciDecrypted\partitions.txt) do (
-echo %%a>>xciDecrypted\helper3.txt
-)
-
 ::Secu order is the order of the secure partitions nca
 for /f "tokens=2* delims=/ " %%a in (xciDecrypted\s_order.txt) do (
 echo %%a>>xciDecrypted\secu_order.txt)
 
+FINDSTR "Number of files:" xciDecrypted\partitions.txt >xciDecrypted\helper3.txt
+for /f "tokens=4* delims= " %%a in (xciDecrypted\helper3.txt) do (
+echo %%a>>xciDecrypted\helper4.txt
+)
 
-Set "skip=,1,2,3,4,6,"
-(for /f "tokens=1,*delims=:" %%a in (' findstr /n "^" ^<xciDecrypted\helper3.txt'
+Set "skip=,1,2,3,5,"
+(for /f "tokens=1,*delims=:" %%a in (' findstr /n "^" ^<xciDecrypted\helper4.txt'
 ) do Echo=%skip%|findstr ",%%a," 2>&1>NUL ||Echo=%%b
 )>xciDecrypted\secu_number.txt
 
@@ -143,11 +174,11 @@ for /f %%a in (xciDecrypted\secu_number.txt) do (
 )
 ::echo !secu_number!>xciDecrypted\secn.txt
 
-
 ::Clean the folder
 del xciDecrypted\helper1.txt
 del xciDecrypted\helper2.txt
 del xciDecrypted\helper3.txt
+del xciDecrypted\helper4.txt
 del xciDecrypted\partitions.txt
 del xciDecrypted\s_order.txt
 
@@ -162,16 +193,6 @@ echo %%a>>xciDecrypted\positions.txt)
 
 del xciDecrypted\helper1.txt
 
-::Get position of .cert
-FINDSTR  /N ".cert" xciDecrypted\secu_order.txt >>xciDecrypted\helper2.txt
-for /f "tokens=1* delims=:" %%a in (xciDecrypted\helper2.txt) do (
-echo %%a>>xciDecrypted\cert_pos.txt)
-del xciDecrypted\helper2.txt
-
-for /f %%a in (xciDecrypted\cert_pos.txt) do (
-    set cert_pos=%%a
-)
-
 ::Get position of cnmt.nca in variables
 set crlt=0
 for /f %%a in (xciDecrypted\positions.txt) do (
@@ -183,8 +204,6 @@ for /f %%a in (xciDecrypted\positions.txt) do (
 ::echo !vet1!>xciDecrypted\vet1.txt
 ::echo !vet2!>xciDecrypted\vet2.txt
 ::echo !vet3!>xciDecrypted\vet3.txt
-::echo !crlt!>xciDecrypted\cont.txt
-::echo !cert_pos!>xciDecrypted\cert_pos2.txt
 ::echo !secu_number!>xciDecrypted\secu_number.txt
 
 
@@ -212,18 +231,22 @@ Set "skip=%skiplist%"
 )>xciDecrypted\game_nca.txt
 
 ::Step 4. Extract secure partition with hactool
-"%~dp0\ztools\hactool.exe" -k "%~dp0\ztools\keys.txt" -t xci --securedir=xciDecrypted\rawsecure\ "%~1"
+echo ------------------------------------------------------------
+echo Extracting secure partition from xci with hactool by SciresM
+echo ------------------------------------------------------------
+"ztools\hactool.exe" -k "ztools\keys.txt" -t xci --securedir=xciDecrypted\rawsecure\ "%~1" >NUL 2>&1
+echo DONE
 MD "xciDecrypted\secure"
 
 ::Step 5. Move game nca to secure
 set game_nca_number=0
 for /f %%a in (xciDecrypted\game_nca.txt) do (
-move  "xciDecrypted\rawsecure\%%a"  "xciDecrypted\secure" 
+move  "xciDecrypted\rawsecure\%%a"  "xciDecrypted\secure" >NUL 2>&1
 set /a game_nca_number+=1
 )
 ::echo !game_nca_number!>xciDecrypted\game_nca_number.txt
 
-echo f | xcopy /f /y "xciDecrypted\secu_order.txt" "xciDecrypted\content_helper.txt"
+echo f | xcopy /f /y "xciDecrypted\secu_order.txt" "xciDecrypted\content_helper.txt" >NUL 2>&1
 set a/ contador=0
 set /a pos2=!vet1!-1
 ::echo %pos2%>xciDecrypted\pos2.txt
@@ -319,12 +342,12 @@ set "skip=%skiplist%"
 MD "xciDecrypted\content!contador!"
 for /f %%a in (xciDecrypted\content!contador!.txt) do (
 move  "xciDecrypted\rawsecure\%%a"  "xciDecrypted\content!contador!" 
-)
+) >NUL 2>&1
 
 set /a pos2=!vet1!
 set /a pos1=1
 set string=
-echo f | xcopy /f /y "xciDecrypted\other_content.txt" "xciDecrypted\content_helper.txt"
+echo f | xcopy /f /y "xciDecrypted\other_content.txt" "xciDecrypted\content_helper.txt" >NUL 2>&1
 del xciDecrypted\other_content.txt
 
 goto :update_list1 
@@ -332,7 +355,7 @@ goto :update_list1
 :endofcontent
 set /a contador+=1
 MD "xciDecrypted\content!contador!"
-move  "xciDecrypted\rawsecure\*"  "xciDecrypted\content!contador!" 
+move  "xciDecrypted\rawsecure\*"  "xciDecrypted\content!contador!" >NUL 2>&1
 
 :nocontent
 
@@ -365,8 +388,10 @@ Set "skip=%gstring%"
 for /f %%a in (xciDecrypted\ncatocheck.txt) do (
     set ncatocheck=%%a
 )
-
-"%~dp0\ztools\hactool.exe" -k "%~dp0\ztools\keys.txt" -t nca -i "xciDecrypted\secure\%ncatocheck%" >"xciDecrypted\nca_data.txt"
+echo -------------------------------------------------
+echo Analyzing nca !c_gamenca! with hactool by SciresM
+echo -------------------------------------------------
+"ztools\hactool.exe" -k "ztools\keys.txt" -t nca -i "xciDecrypted\secure\%ncatocheck%" >"xciDecrypted\nca_data.txt"
 FINDSTR "Type" xciDecrypted\nca_data.txt >xciDecrypted\nca_helper.txt
 for /f "tokens=3* delims=: " %%a in (xciDecrypted\nca_helper.txt) do (
 echo %%a>>xciDecrypted\nca_helper2.txt)
@@ -376,7 +401,8 @@ Set "skip=,2,3,"
 )>xciDecrypted\nca_type.txt
 for /f %%a in (xciDecrypted\nca_type.txt) do (
     set nca_type=%%a
-)
+) >NUL 2>&1
+
 if %nca_type% EQU %mycheck% ( echo %ncatocheck%>>xciDecrypted\manual_list.txt )
 set /a c_gamenca+=1
 del xciDecrypted\ncatocheck.txt
@@ -384,7 +410,7 @@ del xciDecrypted\nca_data.txt
 del xciDecrypted\nca_helper.txt
 del xciDecrypted\nca_helper2.txt
 del xciDecrypted\nca_type.txt
-
+echo is %nca_type%. DONE
 goto getmeinfo1
 
 :getmeinfo2
@@ -398,8 +424,8 @@ for /f %%a in (xciDecrypted\manual_list.txt) do (
 ::del manual_list.txt
 
 ::Set complete route
-set f_tmanual1="%~dp0\xciDecrypted\secure\%tmanual1%"
-set f_tmanual2="%~dp0\xciDecrypted\secure\%tmanual2%"
+set f_tmanual1="xciDecrypted\secure\%tmanual1%"
+set f_tmanual2="xciDecrypted\secure\%tmanual2%"
 
 ::Get size of both nca
 for /f "usebackq" %%A in ('%f_tmanual1%') do set size_tm1=%%~zA
@@ -442,35 +468,46 @@ goto nomanual
 
 :ognm
 ::If only 4 nca just extract secure partition to secure
-"%~dp0\ztools\hactool.exe" -k "%~dp0\ztools\keys.txt" -t xci --securedir=xciDecrypted\secure\ "%~1"
+echo ------------------------------------------------------------
+echo Extracting secure partition from xci with hactool by SciresM
+echo ------------------------------------------------------------
+"ztools\hactool.exe" -k "ztools\keys.txt" -t xci --securedir=xciDecrypted\secure\ "%~1" >NUL 2>&1
+echo DONE
 goto nomanual
 
 ::In case we have no manual to proccess less code
 :nomanual
-
-RD /s /q "%~dp0\xciDecrypted\rawsecure"
+RD /s /q "xciDecrypted\rawsecure" >NUL 2>&1
 ::We don't need to preserve normal and update
 MD "xciDecrypted\update"
 MD "xciDecrypted\normal"
 ::We can clean the txt files
 del xciDecrypted\*.txt
-
 ::Call hacbuild to build the xci
 ::To do->Force hacbuild to build secure in correct order in all cases
 ::To do->Option to preserve padding in secure header for future compatibility to dlc and update reforming
-"%~dp0\ztools\hacbuild.exe" xci_auto "%~dp0\xciDecrypted"  "%~dp0\xciDecrypted\!filename![nxt].xci" 
+echo ------------------------------------------------------
+echo Rebuilding trimmed xci file with hacbuild by LucaFraga
+echo ------------------------------------------------------
+ECHO NOTE:
+echo With files bigger than 4Gb it'll take more time proportionally than with smaller files.
+echo Also you'll need to have at least double the amount of free disk space than file's size.
+echo IF YOU DON'T SEE "DONE" HACBUILD IS STILL AT WORK.
+ECHO ........................................................................................
+"ztools\hacbuild.exe" xci_auto "xciDecrypted" "xciDecrypted\!filename![nxt].xci" >NUL 2>&1
+echo DONE
 
 ::Create output folder and move trimmed file
 echo %filename%>xciDecrypted\fname.txt
 for /f "tokens=1* delims=[" %%a in (xciDecrypted\fname.txt) do (
     set ofolder=%%a)
 del xciDecrypted\fname.txt
-MD "%~dp0\output_nxt\!ofolder!"
-move  "%~dp0\xciDecrypted\*.xci"  "%~dp0\output_nxt\!ofolder!" 
-move  "%~dp0\xciDecrypted\*.nca" "%~dp0\output_nxt\!ofolder!" 
-RD /s /q "%~dp0\xciDecrypted\normal"
-RD /s /q "%~dp0\xciDecrypted\secure"
-RD /s /q "%~dp0\xciDecrypted\update"
+MD "output_nxt\!ofolder!" >NUL 2>&1
+move  "xciDecrypted\*.xci"  "output_nxt\!ofolder!" >NUL 2>&1
+move  "xciDecrypted\*.nca" "output_nxt\!ofolder!" >NUL 2>&1
+RD /s /q "xciDecrypted\normal"
+RD /s /q "xciDecrypted\secure"
+RD /s /q "xciDecrypted\update"
 del xciDecrypted\*.ini
 
 ::Create content nsp
@@ -481,7 +518,7 @@ goto :final
 :create_nsp
 if !contador! LEQ !c_aux! ( goto :final ) else ( set /a c_aux+=1 )
 
-dir "%~dp0\xciDecrypted\content!c_aux!" /b  > "%~dp0\xciDecrypted\fileslist.txt"
+dir "xciDecrypted\content!c_aux!" /b  > "xciDecrypted\fileslist.txt"
 FINDSTR /R /N "^.*" xciDecrypted\fileslist.txt | FIND /C ":">xciDecrypted\fileslist_helper.txt
 for /f %%a in (xciDecrypted\fileslist_helper.txt) do (
     set fileslist=%%a
@@ -491,21 +528,23 @@ set list=0
 for /F "tokens=*" %%A in (xciDecrypted\fileslist.txt) do (
     SET /A list=!list! + 1
     set varlist!list!=%%A
-)
-set varlist
+) >NUL 2>&1
+set varlist >NUL 2>&1
 
 ::echo %string%>xciDecrypted\teststring.txt
-
+echo --------------------------------------------------------------------------
+echo Rebuilding dlc or update [c!c_aux!] as nsp with "nspbuild" by CVFireDragon
+echo --------------------------------------------------------------------------
 if !fileslist! EQU 0 ( goto :final)
-if !fileslist! EQU 1 ( "%~dp0\ztools\nspbuild.py" "%~dp0\xciDecrypted\content!c_aux!.nsp" "%~dp0\xciDecrypted\content!c_aux!\%varlist1%" )
-if !fileslist! EQU 2 ( "%~dp0\ztools\nspbuild.py" "%~dp0\xciDecrypted\content!c_aux!.nsp" "%~dp0\xciDecrypted\content!c_aux!\%varlist1%" "%~dp0\xciDecrypted\content!c_aux!\%varlist2%" )
-if !fileslist! EQU 3 ( "%~dp0\ztools\nspbuild.py" "%~dp0\xciDecrypted\content!c_aux!.nsp" "%~dp0\xciDecrypted\content!c_aux!\%varlist1%" "%~dp0\xciDecrypted\content!c_aux!\%varlist2%" "%~dp0\xciDecrypted\content!c_aux!\%varlist3%" )
-if !fileslist! EQU 4 ( "%~dp0\ztools\nspbuild.py" "%~dp0\xciDecrypted\content!c_aux!.nsp" "%~dp0\xciDecrypted\content!c_aux!\%varlist1%" "%~dp0\xciDecrypted\content!c_aux!\%varlist2%" "%~dp0\xciDecrypted\content!c_aux!\%varlist3%" "%~dp0\xciDecrypted\content!c_aux!\%varlist4%" )
-if !fileslist! EQU 5 ( "%~dp0\ztools\nspbuild.py" "%~dp0\xciDecrypted\content!c_aux!.nsp" "%~dp0\xciDecrypted\content!c_aux!\%varlist1%" "%~dp0\xciDecrypted\content!c_aux!\%varlist2%" "%~dp0\xciDecrypted\content!c_aux!\%varlist3%" "%~dp0\xciDecrypted\content!c_aux!\%varlist4%" "%~dp0\xciDecrypted\content!c_aux!\%varlist5%" )
-if !fileslist! EQU 6 ( "%~dp0\ztools\nspbuild.py" "%~dp0\xciDecrypted\content!c_aux!.nsp" "%~dp0\xciDecrypted\content!c_aux!\%varlist1%" "%~dp0\xciDecrypted\content!c_aux!\%varlist2%" "%~dp0\xciDecrypted\content!c_aux!\%varlist3%" "%~dp0\xciDecrypted\content!c_aux!\%varlist4%" "%~dp0\xciDecrypted\content!c_aux!\%varlist5%" "%~dp0\xciDecrypted\content!c_aux!\%varlist6%")
-if !fileslist! EQU 7 ( "%~dp0\ztools\nspbuild.py" "%~dp0\xciDecrypted\content!c_aux!.nsp" "%~dp0\xciDecrypted\content!c_aux!\%varlist1%" "%~dp0\xciDecrypted\content!c_aux!\%varlist2%" "%~dp0\xciDecrypted\content!c_aux!\%varlist3%" "%~dp0\xciDecrypted\content!c_aux!\%varlist4%" "%~dp0\xciDecrypted\content!c_aux!\%varlist5%" "%~dp0\xciDecrypted\content!c_aux!\%varlist6%" "%~dp0\xciDecrypted\content!c_aux!\%varlist7%")
-if !fileslist! EQU 8 ( "%~dp0\ztools\nspbuild.py" "%~dp0\xciDecrypted\content!c_aux!.nsp" "%~dp0\xciDecrypted\content!c_aux!\%varlist1%" "%~dp0\xciDecrypted\content!c_aux!\%varlist2%" "%~dp0\xciDecrypted\content!c_aux!\%varlist3%" "%~dp0\xciDecrypted\content!c_aux!\%varlist4%" "%~dp0\xciDecrypted\content!c_aux!\%varlist5%" "%~dp0\xciDecrypted\content!c_aux!\%varlist6%" "%~dp0\xciDecrypted\content!c_aux!\%varlist7%" "%~dp0\xciDecrypted\content!c_aux!\%varlist8%")
-
+if !fileslist! EQU 1 ( "ztools\nspbuild.py" "xciDecrypted\content!c_aux!.nsp" "xciDecrypted\content!c_aux!\%varlist1%" ) >NUL 2>&1
+if !fileslist! EQU 2 ( "ztools\nspbuild.py" "xciDecrypted\content!c_aux!.nsp" "xciDecrypted\content!c_aux!\%varlist1%" "xciDecrypted\content!c_aux!\%varlist2%" ) >NUL 2>&1
+if !fileslist! EQU 3 ( "ztools\nspbuild.py" "xciDecrypted\content!c_aux!.nsp" "xciDecrypted\content!c_aux!\%varlist1%" "xciDecrypted\content!c_aux!\%varlist2%" "xciDecrypted\content!c_aux!\%varlist3%" ) >NUL 2>&1
+if !fileslist! EQU 4 ( "ztools\nspbuild.py" "xciDecrypted\content!c_aux!.nsp" "xciDecrypted\content!c_aux!\%varlist1%" "xciDecrypted\content!c_aux!\%varlist2%" "xciDecrypted\content!c_aux!\%varlist3%" "xciDecrypted\content!c_aux!\%varlist4%" ) >NUL 2>&1
+if !fileslist! EQU 5 ( "ztools\nspbuild.py" "xciDecrypted\content!c_aux!.nsp" "xciDecrypted\content!c_aux!\%varlist1%" "xciDecrypted\content!c_aux!\%varlist2%" "xciDecrypted\content!c_aux!\%varlist3%" "xciDecrypted\content!c_aux!\%varlist4%" "xciDecrypted\content!c_aux!\%varlist5%" ) >NUL 2>&1
+if !fileslist! EQU 6 ( "ztools\nspbuild.py" "xciDecrypted\content!c_aux!.nsp" "xciDecrypted\content!c_aux!\%varlist1%" "xciDecrypted\content!c_aux!\%varlist2%" "xciDecrypted\content!c_aux!\%varlist3%" "xciDecrypted\content!c_aux!\%varlist4%" "xciDecrypted\content!c_aux!\%varlist5%" "xciDecrypted\content!c_aux!\%varlist6%") >NUL 2>&1
+if !fileslist! EQU 7 ( "ztools\nspbuild.py" "xciDecrypted\content!c_aux!.nsp" "xciDecrypted\content!c_aux!\%varlist1%" "xciDecrypted\content!c_aux!\%varlist2%" "xciDecrypted\content!c_aux!\%varlist3%" "xciDecrypted\content!c_aux!\%varlist4%" "xciDecrypted\content!c_aux!\%varlist5%" "xciDecrypted\content!c_aux!\%varlist6%" "xciDecrypted\content!c_aux!\%varlist7%") >NUL 2>&1
+if !fileslist! EQU 8 ( "ztools\nspbuild.py" "xciDecrypted\content!c_aux!.nsp" "xciDecrypted\content!c_aux!\%varlist1%" "xciDecrypted\content!c_aux!\%varlist2%" "xciDecrypted\content!c_aux!\%varlist3%" "xciDecrypted\content!c_aux!\%varlist4%" "xciDecrypted\content!c_aux!\%varlist5%" "xciDecrypted\content!c_aux!\%varlist6%" "xciDecrypted\content!c_aux!\%varlist7%" "xciDecrypted\content!c_aux!\%varlist8%") >NUL 2>&1
+echo DONE
 set fileslist=0
 del xciDecrypted\fileslist.txt
 del xciDecrypted\fileslist_helper.txt
@@ -513,21 +552,18 @@ echo %filename%>xciDecrypted\fname.txt
 for /f "tokens=1* delims=[" %%a in (xciDecrypted\fname.txt) do (
     set onsp=%%a)
 del xciDecrypted\fname.txt
-if exist "%~dp0\xciDecrypted\content!c_aux!\*.tik" ( rename "%~dp0\xciDecrypted\content!c_aux!.nsp" "!onsp![c!c_aux!][upd].nsp") else ( rename "%~dp0\xciDecrypted\content!c_aux!.nsp" "!onsp![c!c_aux!][dlc].nsp")
-RD /s /q "%~dp0\xciDecrypted\content!c_aux!"
+if exist "xciDecrypted\content!c_aux!\*.tik" ( rename "xciDecrypted\content!c_aux!.nsp" "!onsp![c!c_aux!][upd].nsp") else ( rename "xciDecrypted\content!c_aux!.nsp" "!onsp![c!c_aux!][dlc].nsp")
+RD /s /q "xciDecrypted\content!c_aux!"
 goto create_nsp
 
 :final
-move  "%~dp0\xciDecrypted\*.nsp" "%~dp0\output_nxt\!ofolder!" 
+move  "xciDecrypted\*.nsp" "output_nxt\!ofolder!" >NUL 2>&1 
 
 ::Delete work folder
-RD /s /q "%~dp0\xciDecrypted"
+RD /s /q "xciDecrypted"
 
-cls
-
-echo Process finished
+echo Processed !filename!
 echo Your files should be in the output_nxt folder
-PING -n 4 127.0.0.1 >NUL 2>&1
 echo    /@
 echo    \ \
 echo  ___\ \
@@ -536,9 +572,10 @@ echo (____@)  \
 echo (____@)   \
 echo (__o)_    \
 echo       \    \
-
+:end
 PING -n 2 127.0.0.1 >NUL 2>&1
-exit
+cls
+
 
 
 
